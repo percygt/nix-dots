c4b9163eba472fe2fb35e90c5a85061ae39c9ab1
Percy Timon <percygt.dev@gmail.com>
2023-12-23 16:18:09 +0800

    🍻

---
+++ program/terminal/alacritty.nix
@@ -0,0 +1,60 @@
+let
+  colors = (import ../../colors.nix).syft;
+in {
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env.TERM = "xterm-256color";
+      window.opacity = colors.alpha;
+      colors = {
+        primary = {
+          background = "#${colors.default.foreground}";
+          foreground = "#${colors.default.background}";
+        };
+        cursor = {
+          text = "#${colors.cursor.foreground}";
+          cursor = "#${colors.cursor.foreground}";
+        };
+        normal = {
+          black = "#${colors.normal.black}";
+          red = "#${colors.normal.red}";
+          green = "#${colors.normal.green}";
+          yellow = "#${colors.normal.yellow}";
+          blue = "#${colors.normal.blue}";
+          magenta = "#${colors.normal.magenta}";
+          cyan = "#${colors.normal.cyan}";
+          white = "#${colors.normal.white}";
+        };
+        bright = {
+          black = "#${colors.bright.black}";
+          red = "#${colors.bright.red}";
+          green = "#${colors.bright.green}";
+          yellow = "#${colors.bright.yellow}";
+          blue = "#${colors.bright.blue}";
+          magenta = "#${colors.bright.magenta}";
+          cyan = "#${colors.bright.cyan}";
+          white = "#${colors.bright.white}";
+        };
+      };
+      font = {
+        normal = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Bold";
+        };
+        italic = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Italic";
+        };
+        bold_italic = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Bold Italic";
+        };
+        size = 11;
+      };
+    };
+  };
+}
