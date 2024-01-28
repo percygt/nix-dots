5d403dce8b4e5fe25b69f800444d1c3ea1b17938
Percy Timon <percygt.dev@gmail.com>
2024-01-28 13:26:13 +0800

    [Neovim] Add comment.nvim

--- common/nvim/lua/config/tools/misc.lua
+++ common/nvim/lua/config/tools/misc.lua
@@ -3,5 +3,6 @@ require("todo-comments").setup({
     TODO = { icon = " ", color = "#de9e00" },
   },
 })
-require("mini.comment").setup()
+-- require("mini.comment").setup()
+require("Comment").setup()
 vim.api.nvim_set_keymap("n", "<leader>gg", ":LazyGit<CR>", { noremap = true, silent = true })
