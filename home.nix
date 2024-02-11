4c9ea083d951bf410fe107f5ce855b4ad7975703
Percy Timon <percygt.dev@gmail.com>
2024-02-11 16:20:48 +0800

    [Multi] Update

--- home.nix
+++ home.nix
@@ -23,14 +23,22 @@ in {
   nix = {
     package = pkgs.nix;
     settings = {
-      experimental-features = ["nix-command" "flakes"];
+      experimental-features = ["nix-command" "flakes" "repl-flake"];
       auto-optimise-store = true;
       warn-dirty = false;
       bash-prompt-prefix = "(nix:$name)\\040";
       max-jobs = "auto";
       extra-nix-path = "nixpkgs=flake:nixpkgs";
-      substituters = [ "https://cache.nixos.org" "https://percygtdev.cachix.org" ];
-      trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "percygtdev.cachix.org-1:AGd4bI4nW7DkJgniWF4tS64EX2uSYIGqjZih2UVoxko="];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://percygtdev.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "percygtdev.cachix.org-1:AGd4bI4nW7DkJgniWF4tS64EX2uSYIGqjZih2UVoxko="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
   };
 
