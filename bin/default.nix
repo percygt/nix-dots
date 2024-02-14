52647af640099b326b013e1e263f9a1abae8d082
Percy Timon <percygt.dev@gmail.com>
2024-02-15 05:14:16 +0800

    Merge nix-dots to main

--- bin/default.nix
+++ bin/default.nix
@@ -1,9 +1,6 @@
-{pkgs}: let
-  gitd = import ./gitd.nix {inherit pkgs;};
-  preview = import ./preview.nix {inherit pkgs;};
-in {
-  home.packages = [
-    gitd
-    preview
+{
+  imports = [
+    ./gitd.nix
+    ./preview.nix
   ];
 }
