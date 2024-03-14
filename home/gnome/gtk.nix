69cb4102196fe0de632192cf28d9c92037a778da
Percy Timon <percygt.dev@gmail.com>
2024-03-14 15:56:11 +0800

    **automated commit**

--- home/gnome/gtk.nix
+++ home/gnome/gtk.nix
@@ -95,13 +95,13 @@ in {
       kora-icon-theme
       sweet
     ];
-    # pointerCursor = {
-    #   package = pkgs.colloid-icon-theme;
-    #   name = CURSOR;
-    #   size = 24;
-    #   gtk.enable = true;
-    #   x11.enable = true;
-    # };
+    pointerCursor = {
+      package = pkgs.colloid-icon-theme;
+      name = CURSOR;
+      size = 24;
+      gtk.enable = true;
+      x11.enable = true;
+    };
     sessionVariables = {
       GTK_THEME = THEME;
       GTK_CURSOR = CURSOR;
