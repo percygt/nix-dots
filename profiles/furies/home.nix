db454e9e4f2f9da01ee842d8cd3232ac1c988bb6
Percy Timon <percygt.dev@gmail.com>
2024-03-14 12:28:41 +0800

    xdg

--- profiles/furies/home.nix
+++ profiles/furies/home.nix
@@ -30,6 +30,10 @@ in {
   };
   xdg = {
     enable = true;
+    configHome = config.home.homeDirectory + "/.config";
+    cacheHome = config.home.homeDirectory + "/.local/cache";
+    dataHome = config.home.homeDirectory + "/.local/share";
+    stateHome = config.home.homeDirectory + "/.local/state";
     mime.enable = true;
     systemDirs.data = ["${config.home.homeDirectory}/.nix-profile/share/applications"];
     configFile.wireplumber = {
