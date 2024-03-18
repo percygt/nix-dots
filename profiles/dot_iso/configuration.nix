ed191e52f5388fbeea95f58472eb2ccf05ae38bd
Percy Timon <percygt.dev@gmail.com>
2024-03-18 13:03:58 +0800

    **automated commit**

--- profiles/dot_iso/configuration.nix
+++ profiles/dot_iso/configuration.nix
@@ -77,8 +77,8 @@ in {
     appendToMenuLabel = " live";
     contents = [
       {
-        source = ~/.ssh/id_ed25519_glab;
-        target = "/id_ed25519_glab";
+        source = ~/.ssh;
+        target = "/ssh";
       }
     ];
   };
@@ -100,7 +100,7 @@ in {
 
         if [ ! -d "${flakeDirectory}/.git" ]; then
           # Define source and destination directories
-          source_dir="/iso/id_ed25519_glab"
+          source_dir="/iso/ssh/."
           destination_dir="/etc/ssh/"
           if [ -e "$source_dir" ]; then
             # Copy .ssh directory to the destination directory
@@ -145,6 +145,7 @@ in {
         # Rsync my nix-config to the target install
         mkdir -p "/mnt/home/${target_user}/nix-dots"
         rsync -a --delete "$DIR/.." "/mnt/home/${target_user}/nix-dots"
+        rsync -a --delete "/iso/id_ed25519_glab" "/mnt/home/${target_user}/nix-dots"
 
         # If there is a keyfile for a data disks, put copy it to the root partition and
         # ensure the permissions are set appropriately.
