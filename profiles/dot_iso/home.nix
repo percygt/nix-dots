9f12290a6d34512c4b8ff020722b42efe3772fa7
Percy Timon <percygt.dev@gmail.com>
2024-03-19 14:41:09 +0800

    **automated commit**

--- profiles/dot_iso/home.nix
+++ profiles/dot_iso/home.nix
@@ -11,7 +11,7 @@
   home = {
     username = lib.mkDefault "nixos";
     homeDirectory = lib.mkDefault "/home/${config.home.username}";
-    stateVersion = lib.mkDefault "23.05";
+    stateVersion = lib.mkDefault "23.11";
   };
   
   nixpkgs = {
