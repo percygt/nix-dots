fadffb4d622a8f93453e95e1299882639b5d9baf
Percy Timon <percygt.dev@gmail.com>
2024-03-19 17:12:24 +0800

    **automated commit**

--- profiles/vm_nixos_hypr/configuration.nix
+++ profiles/vm_nixos_hypr/configuration.nix
@@ -21,12 +21,10 @@ in {
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
   services.xserver = {displayManager.startx.enable = true;};
-  security = {polkit.enable = true;};
 
   # Enabling hyprlnd on NixOS
   programs.hyprland = {
     enable = true;
-    nvidiaPatches = true;
     xwayland.enable = true;
   };
 
