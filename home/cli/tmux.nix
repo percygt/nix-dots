eed09f9107f36c1a89540fe4fef533774f9847cb
Percy Timon <13714574-percygt@users.noreply.gitlab.com>
2024-03-20 16:53:30 +0800

    gitmux

--- home/cli/tmux.nix
+++ home/cli/tmux.nix
@@ -10,7 +10,43 @@ in {
     gitmux
   ];
 
-  xdg.configFile."gitmux/gitmux.conf".source = ../_config/.gitmux.conf;
+  xdg.configFile."gitmux/gitmux.conf".text = ''
+    tmux:
+        symbols:
+            branch: " "
+            hashprefix: ":"
+            ahead: "↑·"
+            behind: "↓·"
+            staged: "✓ "
+            conflict: "✖ "
+            modified: "✶ "
+            untracked: "? "
+            stashed: "≡ "
+            insertions: "Σ"
+            deletions: "✗"
+            clean: " "
+        styles:
+            clear: "#[fg=default]"
+            state: "#[fg=red,bold]"
+            branch: "#[fg=white,bold]"
+            remote: "#[fg=cyan]"
+            divergence: "#[fg=yellow]"
+            staged: "#[fg=green,bold]"
+            conflict: "#[fg=red,bold]"
+            modified: "#[fg=red,bold]"
+            untracked: "#[fg=magenta,bold]"
+            stashed: "#[fg=cyan,bold]"
+            insertions: "#[fg=green]"
+            deletions: "#[fg=red]"
+            clean: "#[fg=blue]"
+        layout: [branch, divergence, " \\ ", flags]
+        options:
+            branch_max_len: 0
+            branch_trim: right
+            ellipsis: …
+            hide_clean: false
+            swap_divergence: false
+  '';
   programs.tmux = {
     enable = true;
     baseIndex = 1;
