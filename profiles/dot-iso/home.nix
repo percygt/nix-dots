edeb16ea304f5ba9da09fb4cfa8d6fbfa90feb14
Percy Timon <percygt.dev@gmail.com>
2024-04-04 05:00:35 +0800

    setup vm

--- profiles/dot-iso/home.nix
+++ profiles/dot-iso/home.nix
@@ -1,4 +1,8 @@
 {
+  pkgs,
+  config,
+  ...
+}: {
   userModules = {
     git = {
       enable = true;
@@ -19,5 +23,16 @@
     fzf.enable = true;
     starship.enable = true;
     yazi.enable = true;
+  };
+
+  sops = {
+    defaultSymlinkPath = "%r/secrets";
+    defaultSecretsMountPoint = "%r/secrets.d";
+  };
+  systemd.user.services.mbsync.Unit.After = ["sops-nix.service"];
+  home = {
+    activation.setupEtc = config.lib.dag.entryAfter ["writeBoundary"] ''
+      /run/current-system/sw/bin/systemctl start --user sops-nix
+    '';
   };
 }
