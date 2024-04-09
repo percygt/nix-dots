ae4fbf1e5705afdacee201ba9200a6c57f697391
Percy Timon <percygt.dev@gmail.com>
2024-04-09 15:25:05 +0800

    setup vm

--- system/desktop/gnome/default.nix
+++ system/desktop/gnome/default.nix
@@ -1,37 +1,36 @@
 {pkgs, ...}: {
-  services.xserver = {
-    enable = true;
-    displayManager.gdm = {
+  services = {
+    flatpak.enable = true;
+    locate.enable = true;
+    printing.enable = true;
+    xserver = {
       enable = true;
-      wayland = true;
+      displayManager.gdm = {
+        enable = true;
+        wayland = true;
+      };
+      displayManager.autoLogin.enable = false;
+      desktopManager.gnome.enable = true;
+      libinput.enable = true;
+      libinput.touchpad.tapping = true; #tap
     };
-    displayManager.autoLogin.enable = false;
-    desktopManager.gnome.enable = true;
-    libinput.enable = true;
-    libinput.touchpad.tapping = true; #tap
   };
-  # Flatpak
-  # services.flatpak.enable = true;
-  # locate
-  # services.locate.enable = true;
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-  # Enable touchpad support (enabled default in most desktopManager).
-  # systemd = {
-  #   user.services.polkit-gnome-authentication-agent-1 = {
-  #     description = "polkit-gnome-authentication-agent-1";
-  #     wantedBy = ["graphical-session.target"];
-  #     wants = ["graphical-session.target"];
-  #     after = ["graphical-session.target"];
-  #     serviceConfig = {
-  #       Type = "simple";
-  #       ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-  #       Restart = "on-failure";
-  #       RestartSec = 1;
-  #       TimeoutStopSec = 10;
-  #     };
-  #   };
-  # };
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
 
   environment.gnome.excludePackages =
     (with pkgs; [
