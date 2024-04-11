099572c278a0780151102255e0e63c7d98a6914d
Percy Timon <percygt.dev@gmail.com>
2024-04-11 10:16:38 +0800

    setup vm

--- profiles/vm-cli/disks.nix
+++ profiles/vm-cli/disks.nix
@@ -33,43 +33,43 @@
             };
           };
         };
-      };
-      lvm_vg = {
-        root_vg = {
-          type = "lvm_vg";
-          lvs = {
-            root = {
-              size = "100%FREE";
-              content = {
-                type = "btrfs";
-                mountpoint = "/";
-                mountOptions = ["defaults"];
-                extraArgs = ["-L" "nixos" "-f"];
-                subvolumes = {
-                  "home" = {
-                    mountOptions = ["compress=lzo"];
-                    mountpoint = "/home";
-                  };
-                  "persist" = {
-                    mountOptions = ["compress=lzo" "noatime"];
-                    mountpoint = "/persist";
-                  };
+        lvm_vg = {
+          root_vg = {
+            type = "lvm_vg";
+            lvs = {
+              root = {
+                size = "100%FREE";
+                content = {
+                  type = "btrfs";
+                  mountpoint = "/";
+                  mountOptions = ["defaults"];
+                  extraArgs = ["-L" "nixos" "-f"];
+                  subvolumes = {
+                    "home" = {
+                      mountOptions = ["compress=lzo"];
+                      mountpoint = "/home";
+                    };
+                    "persist" = {
+                      mountOptions = ["compress=lzo" "noatime"];
+                      mountpoint = "/persist";
+                    };
 
-                  "nix" = {
-                    mountOptions = ["compress=lzo" "noatime"];
-                    mountpoint = "/nix";
+                    "nix" = {
+                      mountOptions = ["compress=lzo" "noatime"];
+                      mountpoint = "/nix";
+                    };
                   };
                 };
               };
-            };
-            windows = {
-              size = "8500M";
-              content = {
-                type = "filesystem";
-                format = "xfs";
-                mountpoint = "/home/percygt/windows";
-                mountOptions = ["defaults"];
-                extraArgs = ["-L" "windows" "-f"];
+              windows = {
+                size = "8500M";
+                content = {
+                  type = "filesystem";
+                  format = "xfs";
+                  mountpoint = "/home/percygt/windows";
+                  mountOptions = ["defaults"];
+                  extraArgs = ["-L" "windows" "-f"];
+                };
               };
             };
           };
