06e4aa6c946bc9a8408228cc7a3c5f6972c7a6d5
Percy Timon <percygt.dev@gmail.com>
2024-04-11 10:20:25 +0800

    Merge branch 'main' of gitlab.com:percygt/nix-dots

--- profiles/vm-cli/disks.nix
+++ profiles/vm-cli/disks.nix
@@ -6,71 +6,69 @@
   };
 
   disko.devices = {
-    disk = {
-      sda = {
-        type = "disk";
-        device = "/dev/vda";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "1024M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = ["umask=0077" "shortname=winnt"];
-              };
+    disk.main = {
+      type = "disk";
+      device = "/dev/vda";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "1024M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = ["umask=0077" "shortname=winnt"];
             };
-            root = {
-              name = "root";
-              size = "100%";
-              content = {
-                type = "lvm_pv";
-                vg = "root_vg";
-              };
+          };
+          root = {
+            name = "root";
+            size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "root_vg";
             };
           };
         };
-        lvm_vg = {
-          root_vg = {
-            type = "lvm_vg";
-            lvs = {
-              root = {
-                size = "100%FREE";
-                content = {
-                  type = "btrfs";
-                  mountpoint = "/";
-                  mountOptions = ["defaults"];
-                  extraArgs = ["-L" "nixos" "-f"];
-                  subvolumes = {
-                    "home" = {
-                      mountOptions = ["compress=lzo"];
-                      mountpoint = "/home";
-                    };
-                    "persist" = {
-                      mountOptions = ["compress=lzo" "noatime"];
-                      mountpoint = "/persist";
-                    };
+      };
+    };
+    lvm_vg = {
+      root_vg = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "btrfs";
+              mountpoint = "/";
+              mountOptions = ["defaults"];
+              extraArgs = ["-L" "nixos" "-f"];
+              subvolumes = {
+                "home" = {
+                  mountOptions = ["compress=lzo"];
+                  mountpoint = "/home";
+                };
+                "persist" = {
+                  mountOptions = ["compress=lzo" "noatime"];
+                  mountpoint = "/persist";
+                };
 
-                    "nix" = {
-                      mountOptions = ["compress=lzo" "noatime"];
-                      mountpoint = "/nix";
-                    };
-                  };
+                "nix" = {
+                  mountOptions = ["compress=lzo" "noatime"];
+                  mountpoint = "/nix";
                 };
               };
-              windows = {
-                size = "8500M";
-                content = {
-                  type = "filesystem";
-                  format = "xfs";
-                  mountpoint = "/home/percygt/windows";
-                  mountOptions = ["defaults"];
-                  extraArgs = ["-L" "windows" "-f"];
-                };
-              };
+            };
+          };
+          windows = {
+            size = "8500M";
+            content = {
+              type = "filesystem";
+              format = "xfs";
+              mountpoint = "/home/percygt/windows";
+              mountOptions = ["defaults"];
+              extraArgs = ["-L" "windows" "-f"];
             };
           };
         };
