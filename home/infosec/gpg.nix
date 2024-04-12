576399428e4387dca97dfed8f126af7af256b396
Percy Timon <percygt.dev@gmail.com>
2024-04-12 19:28:35 +0800

    set ssh-control script

--- home/infosec/gpg.nix
+++ home/infosec/gpg.nix
@@ -5,24 +5,33 @@
   ...
 }: let
   timeout = 432000;
+  gpg-sshid-ctl = pkgs.writeShellApplication {
+    name = "gpg-sshid-ctl";
+    runtimeInputs = with pkgs; [openssh gnupg];
+    text = builtins.readFile ../bin/scripts/gpg-sshid-ctl.sh;
+  };
 in {
   options.infosec = {
     gpg.enable = lib.mkEnableOption "Enable gpg";
   };
 
   config = lib.mkIf config.infosec.gpg.enable {
-    # https://wiki.archlinux.org/title/Paperkey
-    home.packages = with pkgs; [paperkey zbar qrencode];
+    home.packages = lib.mkAfter [gpg-sshid-ctl];
     programs = {
       gpg = {
         enable = true;
         homedir = "${config.xdg.dataHome}/gnupg";
+        mutableKeys = lib.mkDefault false;
       };
       fish.shellInit = ''
         gpg-connect-agent /bye
         export SSH_AUTH_SOCK=$(${config.programs.gpg.package}/bin/gpgconf --list-dirs agent-ssh-socket)
       '';
     };
+
+    # gnome-keyring is greedy and will override SSH_AUTH_SOCK where undesired
+    services.gnome-keyring.enable = lib.mkDefault false;
+
     services.gpg-agent = {
       enableSshSupport = true;
       enableExtraSocket = true;
@@ -31,10 +40,10 @@ in {
       defaultCacheTtl = timeout;
       defaultCacheTtlSsh = timeout;
       enable = true;
-      sshKeys = [
-        "E7FC17A1A41AD93D71B6B5A9853A9AA3ECBFCB53"
-        "0F35FAA58C77270C1FC8CC77D2B107310955DD82"
-      ];
+      # sshKeys = [
+      #   "E7FC17A1A41AD93D71B6B5A9853A9AA3ECBFCB53"
+      #   "0F35FAA58C77270C1FC8CC77D2B107310955DD82"
+      # ];
       pinentryPackage = pkgs.pinentry-gnome3;
       extraConfig = ''
         disable-scdaemon
--- system/infosec/ssh.nix
+++ system/infosec/ssh.nix
@@ -10,12 +10,18 @@
   };
   config = lib.mkIf config.infosec.ssh.enable {
     services.openssh = {
-      enable = true;
+      enable = lib.mkDefault false;
       settings = {
+        KbdInteractiveAuthentication = false;
         PasswordAuthentication = false;
         PermitRootLogin = "no";
       };
     };
+    nix.sshServe.enable = lib.mkDefault false;
+    nix.sshServe.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeVxOzpUCJIUOtSPh46JY0Sz7H37pgzDAKWEcQzVcjY AF4"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMySpo7UqnJPYVICF1gmVtgk5kLNbCvBuzYz8FMNl009 C14"
+    ];
 
     programs.ssh.startAgent = lib.mkForce false;
 
