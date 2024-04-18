850a57613beb33b3c82fa4f9ea99981336794f30
Percy Timon <percygt.dev@gmail.com>
2024-04-18 23:00:27 +0800

    vm-lvm install/reinstall

--- home/browser/firefox.nix
+++ home/browser/firefox.nix
@@ -1,5 +1,16 @@
 {
-  programs.firefox = {
-    enable = true;
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    browser.firefox.enable =
+      lib.mkEnableOption "Enable firefox";
+  };
+
+  config = lib.mkIf config.browser.firefox.enable {
+    programs.firefox = {
+      enable = true;
+    };
   };
 }
