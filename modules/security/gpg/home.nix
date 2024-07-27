85d3b08f6932ccc2460eed20c5b193cf44fc2705
Percy Timon <percygt.dev@gmail.com>
2024-07-27 21:35:33 +0800

    update

--- modules/security/gpg/home.nix
+++ modules/security/gpg/home.nix
@@ -18,6 +18,7 @@ in
         enable = true;
         homedir = "${config.xdg.dataHome}/gnupg";
         mutableKeys = lib.mkDefault false;
+        mutableTrust = lib.mkDefault false;
         scdaemonSettings.disable-ccid = true;
         publicKeys = [
           {
@@ -26,28 +27,6 @@ in
           }
         ];
       };
-      #   fish.shellInit =
-      #     lib.mkIf (config.modules.shell.userDefaultShell == "fish")
-      #       #fish
-      #       ''
-      #         set -e SSH_AGENT_PID
-      #         if test -z $gnupg_SSH_AUTH_SOCK_BY; or test $gnupg_SSH_AUTH_SOCK_BY -ne $fish_pid
-      #             set -gx SSH_AUTH_SOCK (${config.programs.gpg.package}/bin/gpgconf --list-dirs agent-ssh-socket)
-      #         end
-      #         set -gx GPG_TTY (tty)
-      #         ${config.programs.gpg.package}/bin/gpg-connect-agent updatestartuptty /bye >/dev/null
-      #       '';
-      #   bash.profileExtra =
-      #     lib.mkIf (config.modules.shell.userDefaultShell == "bash")
-      #       #bash
-      #       ''
-      #         unset SSH_AGENT_PID
-      #         if [ "''${gnupg_SSH_AUTH_SOCK_by: -0}" -ne $$ ]; then
-      #           export SSH_AUTH_SOCK="$(${config.programs.gpg.package}/bin/gpgconf --list-dirs agent-ssh-socket)"
-      #         fi
-      #         export GPG_TTY=$(tty)
-      #         ${config.programs.gpg.package}/bin/gpg-connect-agent updatestartuptty /bye >/dev/null
-      #       '';
     };
     xdg.dataFile."${config.programs.gpg.homedir}/sshcontrol".source = config.lib.file.mkOutOfStoreSymlink ./sshcontrol;
     services.gpg-agent = {
