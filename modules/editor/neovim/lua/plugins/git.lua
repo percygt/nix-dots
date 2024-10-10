acbb4660772754bb18d8c170ffb3e88c18aedff4
Percy Timon <percygt.dev@gmail.com>
2024-10-11 00:58:23 +0800

    Update

--- modules/editor/neovim/lua/plugins/git.lua
+++ modules/editor/neovim/lua/plugins/git.lua
@@ -16,7 +16,44 @@ return {
       { "<leader>gp", "<cmd>Neogit push<cr>", desc = "Neogit Push" },
       { "<leader>gP", "<cmd>Neogit pull<cr>", desc = "Neogit Pull" },
     },
-    config = true,
+    config = {
+      integrations = {
+        diffview = true,
+        telescope = true,
+      },
+      disable_prompt_on_change = true,
+      kind = "floating",
+      commit_editor = {
+        kind = "floating",
+      },
+      commit_select_view = {
+        kind = "floating",
+      },
+      commit_view = {
+        kind = "floating",
+      },
+      log_view = {
+        kind = "floating",
+      },
+      rebase_editor = {
+        kind = "floating",
+      },
+      reflog_view = {
+        kind = "floating",
+      },
+      merge_editor = {
+        kind = "floating",
+      },
+      tag_editor = {
+        kind = "floating",
+      },
+      preview_buffer = {
+        kind = "floating",
+      },
+      popup = {
+        kind = "floating",
+      },
+    },
   },
 
   { "akinsho/git-conflict.nvim", version = "*", config = true },
