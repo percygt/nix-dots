0e0fd7ac01e42408ea68ae6b1f44adb45ba47cf1
Percy Timon <percygt.dev@gmail.com>
2024-10-11 18:18:29 +0800

    fix(emacs): package/dirvish - use maintained fork

--- modules/editor/emacs/module.nix
+++ modules/editor/emacs/module.nix
@@ -90,6 +90,17 @@ let
     alwaysEnsure = true;
     # config = pkgs.writeText "config.el" (org-tangle-elisp-blocks (builtins.readFile ./init.org));
     config = builtins.readFile emacsConfig;
+    override = final: prev: {
+      ##  use maintained fork
+      dirvish = prev.melpaPackages.dirvish.overrideAttrs (_: {
+        src = pkgs.fetchFromGitHub {
+          owner = "hlissner";
+          repo = "dirvish";
+          rev = "5f046190e886fb0a2dae7e884cc7cd9bcf48ac26";
+          hash = "sha256-VCTbhevhPMVVwBdkT0gdxcSOrWOs4IjdemdZJVDq9W4=";
+        };
+      });
+    };
   };
 
   emacsWithExtraPackages = pkgs.runCommand "emacs" { nativeBuildInputs = [ pkgs.makeWrapper ]; } ''
