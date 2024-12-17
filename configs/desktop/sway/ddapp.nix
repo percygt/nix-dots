da30287fd91f6e0b621f0b926c5108bcbd1208ef
Percy Timon <percygt.dev@gmail.com>
2024-12-17 21:40:45 +0800

    Update ddapp

--- configs/desktop/sway/ddapp.nix
+++ configs/desktop/sway/ddapp.nix
@@ -5,15 +5,19 @@
       #  dropdown/scratchpad app
       # bash
       ''
+        # defaults
         height=100ppt
         width=100ppt
-        while getopts h:w:t:n:c: flag
+        minimize_auto=true
+
+        while getopts h:w:t:n:m:c: flag
         do
           case "''${flag}" in
             h) height=''${OPTARG};;
             w) width=''${OPTARG};;
             t) target=''${OPTARG} ;;
             n) name=''${OPTARG} ;;
+            m) minimize_auto=''${OPTARG} ;;
             c) command=''${OPTARG};;
             *) notify-send "You f*ck up" "''${OPTARG}" ;;
           esac
@@ -26,18 +30,26 @@
           program_data=$( echo $nodes |
             jq "select(.app_id==\"$target\" or .window_properties.class==\"$target\")" )
         fi
+        AUTO_MINIMIZE="/tmp/ddapp_auto_minimize"
         if [[ "$program_data" ]]; then
           id=$( echo "$program_data" | jq ".id" | head -n 1)
           focused=$( echo "$program_data" | jq ".focused" | head -n 1)
           params="[con_id=$id]"
-          visible_floating_win=$(echo $nodes |
-            jq -r "select(((.id == \"$id\")|not) and .visible and (.sticky|not)) | .id")
+          auto_minimize_win=$(echo $nodes |
+            jq -r "select(((.id == \"$id\")|not) and .visible and (.sticky|not)) |
+              (if .app_id then (\"[app_id=\" + .app_id + \"]\") else (\"[class=\" + .window_properties.class + \"]\") end)" |
+                while read app; do
+                  if grep -Fxq "$app" "$AUTO_MINIMIZE"; then
+                    echo "$app"
+                  fi
+                done
+            )
           if [[ "$focused" == "true" ]]; then
             swaymsg "$params move window to scratchpad"
           else
-            if [ -n "$visible_floating_win" ]; then
-              for id in $visible_floating_win; do
-                swaymsg "[con_id=$id] move window to scratchpad"
+            if [ -n "$auto_minimize_win" ]; then
+              for id in $auto_minimize_win; do
+                swaymsg "$param move window to scratchpad"
               done
             fi
             swaymsg "$params move window to workspace current"
@@ -52,12 +64,29 @@
             swaymsg "for_window [app_id=$target] 'move to scratchpad ; scratchpad show ; floating enable ; resize set $height $width ; move position center'"
             swaymsg "for_window [class=$target] 'move to scratchpad ; scratchpad show ; floating enable ; resize set $height $width ; move position center'"
           fi
+
+          if [ "$minimize_auto" = true ] ; then
+            if ! grep -Fxq "[app_id=$target]" "$AUTO_MINIMIZE"; then
+              echo "[app_id=$target]" >> "$AUTO_MINIMIZE"
+            fi
+            if ! grep -Fxq "[class=$target]" "$AUTO_MINIMIZE"; then
+              echo "[class=$target]" >> "$AUTO_MINIMIZE"
+            fi
+          fi
+
           swaymsg exec $command
-          visible_floating_win=$(echo $nodes |
-            jq -r "select(.visible and (.sticky|not)) | .id")
-          if [ -n "$visible_floating_win" ]; then
-            for id in $visible_floating_win; do
-              swaymsg "[con_id=$id] move window to scratchpad"
+          auto_minimize_win=$(echo $nodes |
+            jq -r "select(.visible and (.sticky|not)) |
+              (if .app_id then (\"[app_id=\" + .app_id + \"]\") else (\"[class=\" + .window_properties.class + \"]\") end)" |
+              while read app; do
+                if grep -Fxq "$app" "$AUTO_MINIMIZE"; then
+                  echo "$app"
+                fi
+              done
+            )
+          if [ -n "$auto_minimize_win" ]; then
+            for param in $auto_minimize_win; do
+              swaymsg "$param move window to scratchpad"
             done
           fi
         fi
