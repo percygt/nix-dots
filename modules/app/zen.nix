0911f32d3d8f0c3e0985c0ab2d6103c93856f9ed
Percy Timon <percygt.dev@gmail.com>
2025-05-07 06:30:14 +0800

    auto commit

--- modules/app/zen.nix
+++ modules/app/zen.nix
@@ -6,6 +6,9 @@
 }:
 {
   config = lib.mkIf config.modules.app.zen.enable {
-    home.packages = [ pkgs.zen-browser-twilight ];
+    home.packages = [
+      pkgs.zen-browser-twilight
+      pkgs.vdhcoapp
+    ];
   };
 }
